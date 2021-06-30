48b6a9cc273b3f1663cc122010507c5d5e69c266
Manisha <manishagolane8575@gmail.com>
2021-06-30 14:20:15 +0530

    Add files via upload

---
+++ ReactJS_SpringBoot_RestAPI/Front_end_part_ReactJS/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import UserComponent from './components/UserComponent'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!!!!!!!!</h1>
+      <UserComponent/>
+    </div>
+  );
+}
+
+export default App;
